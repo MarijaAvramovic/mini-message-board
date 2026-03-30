a9d196ecd0545685a7e883f174a37f928ada86c1
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-30 13:16:43 -0400

    deploy db

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,8 @@
+const { configDotenv } = require("dotenv");
 const { Pool } = require("pg");
- 
+
 module.exports = new Pool({
-  connectionString: "postgresql://marijaavramovic@localhost:5432/mini_messageboard",
+  connectionString: process.env.DATABASE_URL
 });
 
 
